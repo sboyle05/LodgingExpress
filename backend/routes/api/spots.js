f52a62605e68cc2e6df249dd40408652c763577d
Sam <sboyle05@gmail.com>
2023-07-10 13:17:14 -0700

    working on get all endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
-const bcrypt = require('bcryptjs');
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User, Spot, SpotImage } = require('../../db/models');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    await Spot
+    const allSpots = await Spot.findAll({})
+    res.json(allSpots)
 })
+
+module.exports = router
